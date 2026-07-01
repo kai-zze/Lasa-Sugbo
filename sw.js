444fb38bb94faabdc8269788f9618c33c6deaad7
Mon <slmnconstantino@gmail.com>
2026-07-01 12:02:54 +0800

    Optimize font loading, register service worker, and improve performance

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'lasa-sugbo-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './css/style.min.css',
+  './js/script.js',
+  './fonts/fraunces.woff2',
+  './fonts/spacemono-regular.woff2',
+  './fonts/spacemono-bold.woff2',
+  './fonts/worksans.woff2'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
